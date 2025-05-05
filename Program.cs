9a69187cc1c8f663fbc2d662554d5f901e2bc013
Beatriz Batista <byah24@outlook.com>
2025-05-05 09:08:35 -0300

    Programa Finalizado

--- Program.cs
+++ Program.cs
@@ -1,37 +1,81 @@
 ﻿using System.Diagnostics;
 
 Console.Clear();
-double P1,P2,P3,L1,L2,L3,L4,L5,L6, media, mediaListas, mediaProvas, mediaFinal;
+double P1,P2,P3,L1,L2,L3,L4,L5,L6, mediaListas, mediaProvas, mediaFinal;
 const double MEDIA_APROVACAO = 6.0;
 
 
 Console.Write("Informe a nota da L1:");
 L1 = Convert.ToDouble(Console.ReadLine());
 
+if (L1 < 0 || L1 > 10)
+{
+    Console.WriteLine("Nota da lista 01 inválida!");
+    return;
+}
+
 
 Console.Write("Informe a nota da L2:");
 L2 = Convert.ToDouble(Console.ReadLine());
 
+/*
+-inf             0          10          +inf
+|----------------|-----------|------------|
+*/
+
+if (!(L2 >= 0 && L2 <= 10))
+{
+    Console.WriteLine("Nota da lista 02 inválida!");
+    return;  
+}
+
 
 Console.Write("Informe a nota da L3:");
 L3 = Convert.ToDouble(Console.ReadLine());
+if (L3 < 0 || L3 > 10)
+{
+    Console.WriteLine("Nota da lista 03 inválida!");
+    return;
+}
 
 Console.Write("Informe a nota da L4:");
 L4 = Convert.ToDouble(Console.ReadLine());
+if (L4< 0 || L4 > 10)
+{
+    Console.WriteLine("Nota da lista 04 inválida!");
+    return;
+}
 
 
 Console.Write("Informe a nota da L5:");
 L5 = Convert.ToDouble(Console.ReadLine());
 
+if (L5 < 0 || L5 > 10)
+{
+    Console.WriteLine("Nota da lista 05 inválida!");
+    return;
+}
 
 Console.Write("Informe a nota da L6:");
 L6 = Convert.ToDouble(Console.ReadLine());
+
+if (L6 < 0 || L6 > 10)
+{
+    Console.WriteLine("Nota da lista 06 inválida!");
+    return;
+}
 
 Console.Write("Informe a nota da P1:");
 P1 = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Informe a nota da P2:");
 P2 = Convert.ToDouble(Console.ReadLine());
+
+if (P1 < 0 || P1 > 10 || P2 < 0 || P2 > 10)
+{
+   Console.WriteLine("P1 ou P2 com valor inválido");
+   return; 
+}
 
 mediaListas = (L1 + L2 + L3 + L4 + L5 + L6) / 6 * 0.2;
 mediaProvas = (P1 + P2) / 2 * 0.8 ;
@@ -54,6 +98,13 @@ else
 {
     Console.Write("Informe a nota da P3:");
     P3 = Convert.ToDouble(Console.ReadLine());
+
+    if (P3 < 0 || P3 > 10)
+    {
+        Console.WriteLine("P3 com valor inválido!");
+        return;
+    }
+
     
     mediaProvas = (Math.Max(P1, P2) + P3) / 2 * 0.8;
     mediaFinal = mediaListas + mediaProvas;
